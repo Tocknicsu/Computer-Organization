df9b19db6720bd65cb3469731b645922f9ca70e1
Tocknicsu <wingemerald@gmail.com>
2016-05-12 22:03:53 +0800

    update lab3

---
+++ lab3/ALU_Ctrl.v
@@ -0,0 +1,56 @@
+//Subject:     CO project 2 - ALU Controller
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+
+module ALU_Ctrl(
+          funct_i,
+          ALUOp_i,
+          ALUCtrl_o
+          );
+          
+//I/O ports 
+input      [6-1:0] funct_i;
+input      [3-1:0] ALUOp_i;
+
+output     [4-1:0] ALUCtrl_o;    
+     
+//Internal Signals
+reg        [4-1:0] ALUCtrl_o;
+
+//Parameter
+
+       
+//Select exact operation
+always @(*) begin
+    casez ({ALUOp_i, funct_i})
+        9'b000??????: ALUCtrl_o = 4'b0010;  //Addi
+        9'b110??????: ALUCtrl_o = 4'b0110;  //BEQ, BNE
+        9'b111??????: ALUCtrl_o = 4'b0111;  //SLTIU
+        9'b001??????: ALUCtrl_o = 4'b0001;  //ORI
+        9'b100??????: ALUCtrl_o = 4'b1110;  //LUI
+        /* R-Type */
+        9'b010100101: ALUCtrl_o = 4'b0001;  //Or
+        9'b010100000: ALUCtrl_o = 4'b0010;  //Add
+        9'b010101010: ALUCtrl_o = 4'b0111;  //SLT
+        9'b010100100: ALUCtrl_o = 4'b0000;  //AND
+        9'b010100010: ALUCtrl_o = 4'b0110;  //SUB
+        9'b010000011: ALUCtrl_o = 4'b1010;  //SRA
+        9'b010000111: ALUCtrl_o = 4'b1010;  //SRAV
+    endcase
+end
+
+endmodule     
+
+
+
+
+
+                    
+                    
---
+++ lab3/Decoder.v
@@ -0,0 +1,121 @@
+//Subject:     CO project 2 - Decoder
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+
+module Decoder(
+    instr_op_i,
+    funct_i,
+	RegWrite_o,
+	ALU_op_o,
+    ALUSrc1_o,
+	ALUSrc2_o,
+	RegDst_o,
+	Branch_o,
+    Jump_o,
+    ImmExtensionSelect_o,
+    ExtensionSelect_o,
+    ALUZeroSelect_o
+	);
+     
+//I/O ports
+input  [6-1:0] instr_op_i;
+input  [6-1:0] funct_i;
+
+output         RegWrite_o;
+output [3-1:0] ALU_op_o;
+output         ALUSrc1_o;
+output         ALUSrc2_o;
+output         RegDst_o;
+output         Branch_o;
+output         Jump_o;
+output         ImmExtensionSelect_o;
+output         ExtensionSelect_o;
+output         ALUZeroSelect_o;
+ 
+//Internal Signals
+
+reg zero = 0;
+
+
+//Parameter
+wire addi;
+wire rtype;
+wire beq;
+wire sltiu;
+wire ori;
+wire lui;
+wire sra;
+wire srav;
+wire bne;
+wire jump;
+
+assign addi = instr_op_i == 6'b001000;
+assign rtype = instr_op_i == 6'b000000 & (funct_i != 3) & (funct_i != 7);
+assign beq = instr_op_i == 6'b000100;
+assign sltiu = instr_op_i == 6'b001001;
+assign ori = instr_op_i == 6'b001101;
+assign lui = instr_op_i == 6'b001111;
+assign sra = instr_op_i == 6'b000000 & (funct_i == 3);
+assign srav = instr_op_i == 6'b000000 & (funct_i == 7);
+assign bne = instr_op_i == 6'b000101;
+assign jump = instr_op_i == 6'b000010;
+
+assign ALUSrc1_o = sra;
+assign ALUSrc2_o = addi | sltiu | ori | lui | sra;
+assign RegWrite_o = addi | rtype | sltiu | ori | lui | sra | srav;
+assign RegDst_o = rtype | sra | srav;
+assign Branch_o = beq | bne;
+assign Jump_o = jump;
+assign ImmExtensionSelect_o = sltiu | ori;
+assign ExtensionSelect_o = sra;
+assign ALUZeroSelect_o = bne;
+
+assign ALU_op_o[2] = beq | sltiu | lui | bne;
+assign ALU_op_o[1] = rtype | beq | sltiu | sra | srav | bne;
+assign ALU_op_o[0] = sltiu | ori;
+
+/*
+addi: 000
+RType: 010
+BEQ: 110
+SLTIU: 111
+ori: 001
+lui: 100
+sra: 010
+srav: 010
+bne: 110
+*/
+
+//Main function
+/*
+always @(*) begin
+    case (instr_op_i) 
+            end
+        end
+            end
+        6'b101011: //sw
+            begin
+                RegDst_o = 0; 
+                RegWrite_o = 0;
+                ALU_op_o = 3'b000;
+                ALUSrc1_o = 0;
+                ALUSrc2_o = 0;
+                Branch_o = 0;
+                Jump_o = 0;
+                ImmExtensionSelect_o = 0;
+                ExtensionSelect_o = 0;
+                ALUZeroSelect_o = 0;
+            end
+        default: ;
+    endcase
+end
+*/
+
+endmodule
